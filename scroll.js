664298aa7942fe12305659b5fbcbbfab6bfa0c7f
Jemila Al-Ghani <jalghani@jemilas-mbp.attlocal.net>
2019-02-20 01:11:17 -0800

    styling about and menu

--- scroll.js
+++ scroll.js
@@ -1,17 +1,20 @@
+let movement = 0;
 window.onscroll = function() {
+  movement += 1;
   header();
+  body(movement);
 };
 
 function header() {
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("logo").className = "scroll-logo";
     document.getElementById("logo").style.width = "100px";
     let nav = document.getElementsByClassName("nav");
     for (let i = 0; i < nav.length; i++) {
       nav[i].style["font-size"] = "10px";
-      nav[i].style["bottom"] = "20px";
-      nav[i].style["transition"] = "font-size .5s";
-      nav[i].style["transition"] = "bottom .5s";
+      nav[i].style["bottom"] = "25px";
+      nav[i].style["transition"] = "font-size .3s";
+      nav[i].style["transition"] = "bottom .3s";
     }
   } else {
     document.getElementById("logo").style.width = "150px";
@@ -20,8 +23,17 @@ function header() {
     for (let i = 0; i < nav.length; i++) {
       nav[i].style["font-size"] = "15px";
       nav[i].style["bottom"] = "28px";
-      nav[i].style["transition"] = "font-size .5s";
-      nav[i].style["transition"] = "bottom .5s";
+      nav[i].style["transition"] = "font-size .3s";
+      nav[i].style["transition"] = "bottom .3s";
     }
+  }
+}
+function body(x) {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    let videos = document.getElementsByClassName("video-box");
+    for (let i = 0; i < videos.length; i++) {
+      console.log(i, videos[i].offsetTop);
+    }
+  } else {
   }
 }
